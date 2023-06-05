a3df3a8efdbd2122b0232cfd34a129aac0ac0c5b
XhmikosR <xhmikosr@gmail.com>
2023-06-05 14:26:36 +0300

    Switch to async, update file-type, add zst, require Node.js 16 (#1)

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+import {Buffer} from 'node:buffer';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
@@ -6,12 +7,33 @@ import archiveType from '../index.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-test('works', async t => {
-	const tar = await fs.readFile(path.join(__dirname, 'fixtures/test.tar'));
-	const zip = await fs.readFile(path.join(__dirname, 'fixtures/test.zip'));
+test('tar', async t => {
+	const buf = await fs.readFile(path.join(__dirname, 'fixtures/test.tar'));
+	const {ext, mime} = await archiveType(buf);
 
-	t.is(archiveType(tar).ext, 'tar');
-	t.is(archiveType(tar).mime, 'application/x-tar');
-	t.is(archiveType(zip).ext, 'zip');
-	t.is(archiveType(zip).mime, 'application/zip');
+	t.is(ext, 'tar');
+	t.is(mime, 'application/x-tar');
+});
+
+test('zip', async t => {
+	const buf = await fs.readFile(path.join(__dirname, 'fixtures/test.zip'));
+	const {ext, mime} = await archiveType(buf);
+
+	t.is(ext, 'zip');
+	t.is(mime, 'application/zip');
+});
+
+test('zst', async t => {
+	const buf = await fs.readFile(path.join(__dirname, 'fixtures/test.zst'));
+	const {ext, mime} = await archiveType(buf);
+
+	t.is(ext, 'zst');
+	t.is(mime, 'application/zstd');
+});
+
+test('invalid', async t => {
+	const buf = Buffer.from('invalid');
+	const type = await archiveType(buf);
+
+	t.is(type, null);
 });
